152ed407d1cf00097b71825fa6363b08b808a577
adamhaney <adam.haney@retickr.com>
2012-04-13 18:32:13 +0000

    * Incremented version number

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     name="retickrtools",
     author="Adam Haney",
     author_email="adam.haney@retickr.com",
-    version="0.1.2",
+    version="0.1.3",
     description=("A collection of tools used for common idioms at retickr"),
     license="Closed",
     keywords="Data Model",
