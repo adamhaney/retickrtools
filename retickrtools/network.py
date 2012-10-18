a271bb346720aa71eb1c4a18a6b1db2030468c77
Adam Haney <adam.haney@retickr.com>
2012-10-18 17:46:07 +0000

    * Added caching to event_network

--- retickrtools/network.py
+++ retickrtools/network.py
@@ -1,9 +1,26 @@
+import hashlib
 import eventlet
 import eventlet.timeout
 from eventlet.green import urllib2, httplib
 
 
-def event_network(uris, timeout=15, greenpoolsize=1000, greenpool=None, headers=None, json=False, default_value=""):
+def md5(str_):
+    md5 = hashlib.md5()    
+    md5.update(str_)
+    return md5.hexdigest()
+
+
+def event_network(
+    uris,
+    timeout=15,
+    greenpoolsize=1000,
+    greenpool=None,
+    headers=None,
+    json=False,
+    default_value="",
+    cache=None,
+    cache_key="event_network_cache",
+    cache_length=60):
     """
     Given a list of uris to pull over network pull them and then
     return a dictionary of their responses keyed on the uri which was
@@ -17,6 +34,14 @@ def event_network(uris, timeout=15, greenpoolsize=1000, greenpool=None, headers=
         pool = greenpool
 
     def pull_link(link):
+
+        if cache != None:
+
+            # Check for the response in cache
+            response = cache.get("{0}::{1}".format(cache_key, md5(link)))
+            if response != None:
+                return link, response
+
         with eventlet.timeout.Timeout(timeout, False) as timeout:
             try:
                 req = urllib2.Request(link)
@@ -25,7 +50,10 @@ def event_network(uris, timeout=15, greenpoolsize=1000, greenpool=None, headers=
                     for k, v in headers.items():
                         req.add_header(k, v)
                         
-                    return (link, urllib2.urlopen(req).read())
+                response = urllib2.urlopen(req).read()
+
+                cache.set("{0}::{1}".format(cache_key, md5(link), response, cache_length))
+                return (link, )
 
             except (eventlet.Timeout, urllib2.HTTPError, httplib.BadStatusLine):
                 return (link, default_value)
